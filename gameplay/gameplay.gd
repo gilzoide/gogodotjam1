ccf0bc0fee3e5b0911ea910666522ba33d37c7ef
gilzoide <gilzoide@gmail.com>
2021-05-30 09:46:49 -0300

    Update ball to move

--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -1,7 +1,9 @@
 extends Control
 
+export(Vector2) var player_start = Vector2(0.5, 0.75)
+
 onready var _player = $Ball
 
 
 func _ready() -> void:
-	_player.position = rect_size * 0.5
+	_player.position = rect_size * player_start
--- player/ball.gd
+++ player/ball.gd
@@ -1,11 +1,12 @@
-tool
 extends KinematicBody2D
 
-export(Texture) var texture
+export(float) var speed = 100
+export(float) var angular_speed = -2
 export(float) var size = 100
 export(float) var min_size = 50
 export(float) var max_size = 1000
 
+var _movement = Vector2.LEFT
 onready var _sprite = $Sprite
 
 
@@ -14,8 +15,14 @@ func _input(event: InputEvent) -> void:
 		set_size(size + event.relative)
 
 
+func _process(delta: float) -> void:
+	_sprite.rotate(angular_speed * delta)
+
+
 func _physics_process(delta: float) -> void:
-	pass
+	var collision = move_and_collide(_movement * speed * delta)
+	if collision:
+		_movement = collision.normal
 
 
 func set_size(value: float) -> void:
