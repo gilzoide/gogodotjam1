e3cf32cd65ce81ea505d85f4a4cb1811638f8902
gilzoide <gilzoide@gmail.com>
2021-05-31 10:51:12 -0300

    Add mute button

--- gameplay/pause_menu.gd
+++ gameplay/pause_menu.gd
@@ -1,5 +1,13 @@
-extends PanelContainer
+extends Control
+
+onready var _mute_button = $VBoxContainer/MuteButton
 
 
 func _on_meta_clicked(meta) -> void:
 	var _err = OS.shell_open(meta)
+
+
+func _on_MuteButton_toggled(button_pressed: bool) -> void:
+	var master_idx = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(master_idx, button_pressed)
+	_mute_button.text = "🔇" if button_pressed else "🔊"
