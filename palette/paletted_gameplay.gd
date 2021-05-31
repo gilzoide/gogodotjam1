caa1bdb407db228758d7c901a7f0fff1def4b795
gilzoide <gilzoide@gmail.com>
2021-05-31 20:58:49 -0300

    Animate color palette change with a tween

--- palette/paletted_gameplay.gd
+++ palette/paletted_gameplay.gd
@@ -11,10 +11,15 @@ const PALETTES = PoolColorArray([
 ])
 
 export(Resource) var settings = preload("res://settings/default_settings.tres")
+export(float) var tween_duration = 0.25
 
 var _current_palette = 0
+var _foreground = Color.white
+var _background = Color.black
 
+onready var _tween = $Tween
 onready var _viewport = $Viewport
+
 
 func _ready() -> void:
 	var _err = settings.connect("changed", self, "_on_settings_changed")
@@ -28,18 +33,32 @@ func _input(event: InputEvent) -> void:
 
 func _on_settings_changed() -> void:
 	_current_palette = settings.palette_index % PALETTES.size()
-	_update_shader()
+	_update_shader(Engine.get_idle_frames() > 0)
 
 
-func _update_shader() -> void:
+func _update_shader(animated = true) -> void:
 	var is_inverted = _current_palette % 2
-	var foreground
-	var background
-	if is_inverted:
-		foreground = PALETTES[_current_palette]
-		background = PALETTES[_current_palette - 1]
-	else:
-		foreground = PALETTES[_current_palette]
-		background = PALETTES[_current_palette + 1]
-	material.set_shader_param("foreground", foreground)
-	material.set_shader_param("background", background)
+	var new_foreground = PALETTES[_current_palette]
+	var new_background = PALETTES[_current_palette + (-1 if is_inverted else 1)]
+	_tween.stop_all()
+	_tween.interpolate_method(
+		self, "_set_foreground",
+		_foreground, new_foreground,
+		tween_duration * float(animated), Tween.TRANS_QUAD, Tween.EASE_IN
+	)
+	_tween.interpolate_method(
+		self, "_set_background",
+		_background, new_background,
+		tween_duration * float(animated), Tween.TRANS_QUAD, Tween.EASE_IN
+	)
+	_tween.start()
+	_foreground = new_foreground
+	_background = new_background
+
+
+func _set_foreground(color: Color) -> void:
+	material.set_shader_param("foreground", color)
+
+
+func _set_background(color: Color) -> void:
+	material.set_shader_param("background", color)
